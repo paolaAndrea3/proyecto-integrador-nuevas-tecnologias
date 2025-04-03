89daafec28b58e5708c4a6cc9ac6689e19499e48
paolaAndrea3 <murillojimenezpaola1@gmail.com>
2025-04-02 22:30:16 -0500

    Se agregan  las actividades 1-2-3-4

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 # Configuración de la página
 st.set_page_config(   
@@ -24,3 +25,115 @@ st.markdown("""
 
 st.header("Solución")
 
+
+# Título y descripción en Streamlit
+st.title("Actividad 1 - Diccionario con DataFrame")
+libros = {
+    "Título": ["Crónica de una muerte anunciada", "El hobbit", "Matar a un ruiseñor"],
+    "Autor": ["Gabriel García Márquez", "J.R.R. Tolkien", "Harper Lee"],
+    "Año de Publicación": [1981, 1937, 1960],
+    "Género": ["Realismo mágico", "Fantasía", "Drama"]
+}
+
+df_libros = pd.DataFrame(libros)
+st.write("DataFrame de Libros")
+st.dataframe(df_libros)
+
+st.write("Código que genera el DataFrame de libros:")
+code="""" 
+libros = {
+"Título": ["Crónica de una muerte anunciada", "El hobbit", "Matar a un ruiseñor"],
+"Autor": ["Gabriel García Márquez", "J.R.R. Tolkien", "Harper Lee"],
+"Año de Publicación": [1981, 1937, 1960],
+"Género": ["Realismo mágico", "Fantasía", "Drama"]
+}
+
+df_libros = pd.DataFrame(libros)
+st.write("DataFrame de Libros")
+st.dataframe(df_libros)
+"""
+st.code(code, language='python')
+
+st.title("Actividad 2 - Lista de diccionarios")
+
+Ciudades = [
+    {"Nombre": "Medellin", "Poblacion": 2500000, "País": "Colombia"},
+    {"Nombre": "Madrid", "Poblacion": 3200000, "País": "España"},
+    {"Nombre": "Cartagena", "Poblacion": 1065881, "País": "Colombia"},
+    {"Nombre": "Barranquilla", "Poblacion": 1320000, "País": "Colombia"},
+]
+df_Ciudades = pd.DataFrame(Ciudades)
+st.write("DataFrame de Ciudades")
+st.dataframe(df_Ciudades)
+st.write("Código que genera el DataFrame de ciudades:")
+code = """
+Ciudades = [
+    {"Nombre": "Medellin", "Poblacion": 2500000, "País": "Colombia"},
+    {"Nombre": "Madrid", "Poblacion": 3200000, "País": "España"},
+    {"Nombre": "Cartagena", "Poblacion": 1065881, "País": "Colombia"},
+    {"Nombre": "Barranquilla", "Poblacion": 1320000, "País": "Colombia"},
+]
+df_Ciudades = pd.DataFrame(Ciudades)
+st.write("DataFrame de Ciudades")
+st.dataframe(df_Ciudades)
+"""
+st.code(code, language='python')
+
+st.title("Actividad 3 - Lista de listas")
+productos = [
+    ["Manzana", 2600,100],
+    ["Pantalones", 45000, 50],
+    ["Galletas", 134,20],
+    ["Arroz", 2300, 200],
+]
+df_Productos = pd.DataFrame(productos, columns=["Nombre", "Precio", "Stock"])
+st.write("DataFrame de Productos")
+st.dataframe(df_Productos)
+st.write("Código que genera el DataFrame de productos:")
+code = """
+productos = [
+    ["Manzana", 2600,100],
+    ["Pantalones", 45000, 50],
+    ["Galletas", 134,20],
+    ["Arroz", 2300, 200],
+]
+df_Productos = pd.DataFrame(productos, columns=["Nombre", "Precio", "Stock"])
+st.write("DataFrame de Productos")
+st.dataframe(df_Productos)
+"""
+st.code(code, language='python')
+
+st.title("Actividad 4 - Series")
+
+nombres = pd.Series(["Rosa", "Omaira", "Paola", "Dario"])
+edades = pd.Series([67, 48, 18, 70])
+ciudades = pd.Series(["Medellin", "Medellin", "Medellin", "Medellin"])
+
+datos_personas = {
+    "Nombre": nombres,
+    "Edad": edades,
+    "Ciudad": ciudades
+}
+# Crear un DataFrame a partir del diccionario
+df_personas = pd.DataFrame(datos_personas)
+st.write("Datos de Personas")
+st.dataframe(df_personas)
+
+st.write("Código que genera el DataFrame de personas")
+code = """" 
+
+nombres = pd.Series(["Rosa", "Omaira", "Paola", "Dario"])
+edades = pd.Series([67, 48, 18, 70])
+ciudades = pd.Series(["Medellin", "Medellin", "Medellin", "Medellin"])
+
+datos_personas = {
+    "Nombre": nombres,
+    "Edad": edades,
+    "Ciudad": ciudades
+}
+# Crear un DataFrame a partir del diccionario
+df_personas = pd.DataFrame(datos_personas)
+st.write("Datos de Personas")
+st.dataframe(df_personas)
+"""
+st.code(code, language='python')
